965348105a528918cda50d1a7bced9bb257ce8c7
Kashima Takumi <kodokurider@gmail.com>
2014-04-01 07:55:38 +0900

    トーンカーブのフィルタを追加しました

--- FilterTest/MainViewController.h
+++ FilterTest/MainViewController.h
@@ -39,6 +39,40 @@
 - (IBAction)colorControlBrightnessChangedValue:(id)sender;
 - (IBAction)colorControlContrastChangedValue:(id)sender;
 
+// Tone curve
+@property (strong, nonatomic) IBOutlet UISlider *toneCurveInput0XSlider;
+@property (strong, nonatomic) IBOutlet UISlider *toneCurveInput0YSlider;
+@property (strong, nonatomic) IBOutlet UILabel *toneCurveInput0XLabel;
+@property (strong, nonatomic) IBOutlet UILabel *toneCurveInput0YLabel;
+@property (strong, nonatomic) IBOutlet UISlider *toneCurveInput1XSlider;
+@property (strong, nonatomic) IBOutlet UISlider *toneCurveInput1YSlider;
+@property (strong, nonatomic) IBOutlet UILabel *toneCurveInput1XLabel;
+@property (strong, nonatomic) IBOutlet UILabel *toneCurveInput1YLabel;
+@property (strong, nonatomic) IBOutlet UISlider *toneCurveInput2XSlider;
+@property (strong, nonatomic) IBOutlet UISlider *toneCurveInput2YSlider;
+@property (strong, nonatomic) IBOutlet UILabel *toneCurveInput2XLabel;
+@property (strong, nonatomic) IBOutlet UILabel *toneCurveInput2YLabel;
+@property (strong, nonatomic) IBOutlet UISlider *toneCurveInput3XSlider;
+@property (strong, nonatomic) IBOutlet UISlider *toneCurveInput3YSlider;
+@property (strong, nonatomic) IBOutlet UILabel *toneCurveInput3XLabel;
+@property (strong, nonatomic) IBOutlet UILabel *toneCurveInput3YLabel;
+@property (strong, nonatomic) IBOutlet UISlider *toneCurveInput4XSlider;
+@property (strong, nonatomic) IBOutlet UISlider *toneCurveInput4YSlider;
+@property (strong, nonatomic) IBOutlet UILabel *toneCurveInput4XLabel;
+@property (strong, nonatomic) IBOutlet UILabel *toneCurveInput4YLabel;
+- (IBAction)toneCurveChangedCheck:(id)sender;
+- (IBAction)toneCurveInput0XChangedValue:(id)sender;
+- (IBAction)toneCurveInput0YChangedValue:(id)sender;
+- (IBAction)toneCurveInput1XChangedValue:(id)sender;
+- (IBAction)toneCurveInput1YChangedValue:(id)sender;
+- (IBAction)toneCurveInput2XChangedValue:(id)sender;
+- (IBAction)toneCurveInput2YChangedValue:(id)sender;
+- (IBAction)toneCurveInput3XChangedValue:(id)sender;
+- (IBAction)toneCurveInput3YChangedValue:(id)sender;
+- (IBAction)toneCurveInput4XChangedValue:(id)sender;
+- (IBAction)toneCurveInput4YChangedValue:(id)sender;
+
+
 // ScrollView関連
 @property (strong, nonatomic) IBOutlet UIView *contentView;
 @property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
--- FilterTest/MainViewController.m
+++ FilterTest/MainViewController.m
@@ -14,6 +14,7 @@
 @property (nonatomic, assign) BOOL isSepiaEnabled;
 @property (nonatomic, assign) BOOL isGrayScaleEnabled;
 @property (nonatomic, assign) BOOL isColorControlEnabled;
+@property (nonatomic, assign) BOOL isToneCurveEnabled;
 @end
 
 @implementation MainViewController
@@ -39,6 +40,7 @@
     self.isSepiaEnabled = NO;
     self.isGrayScaleEnabled = NO;
     self.isColorControlEnabled = NO;
+    self.isToneCurveEnabled = NO;
     
     [self filterImage];
 }
@@ -113,6 +115,65 @@
     [self filterImage];
 }
 
+- (IBAction)toneCurveChangedCheck:(id)sender
+{
+    UISwitch *_switch = (UISwitch *)sender;
+    self.isToneCurveEnabled = _switch.on;
+    
+    [self filterImage];
+}
+
+- (IBAction)toneCurveInput0XChangedValue:(id)sender
+{
+    [self filterImage];
+}
+
+- (IBAction)toneCurveInput0YChangedValue:(id)sender
+{
+    [self filterImage];
+}
+
+- (IBAction)toneCurveInput1XChangedValue:(id)sender
+{
+    [self filterImage];
+}
+
+- (IBAction)toneCurveInput1YChangedValue:(id)sender
+{
+    [self filterImage];
+}
+
+- (IBAction)toneCurveInput2XChangedValue:(id)sender
+{
+    [self filterImage];
+}
+
+- (IBAction)toneCurveInput2YChangedValue:(id)sender
+{
+    [self filterImage];
+}
+
+- (IBAction)toneCurveInput3XChangedValue:(id)sender
+{
+    [self filterImage];
+}
+
+- (IBAction)toneCurveInput3YChangedValue:(id)sender
+{
+    [self filterImage];
+}
+
+- (IBAction)toneCurveInput4XChangedValue:(id)sender
+{
+    [self filterImage];
+}
+
+- (IBAction)toneCurveInput4YChangedValue:(id)sender
+{
+    [self filterImage];
+}
+
+
 - (void)filterImage
 {
     UIImage *filteredImage = [_originalImage copy];
@@ -142,6 +203,44 @@
     _colorControlContrastLabel.text = [NSString stringWithFormat:@"%f", colorControlContrastValue];
     if (_isColorControlEnabled) {
         filteredImage = [TTKEditImage imageFilterColorAdjustment:filteredImage withSaturation:colorControlSaturationValue brightness:colorControlBrightnessValue contrast:colorControlContrastValue];
+    }
+    
+    // Tone curve
+    CGFloat toneCurveInput0XValue = _toneCurveInput0XSlider.value;
+    _toneCurveInput0XLabel.text = [NSString stringWithFormat:@"%f", toneCurveInput0XValue];
+    CGFloat toneCurveInput0YValue = _toneCurveInput0YSlider.value;
+    _toneCurveInput0YLabel.text = [NSString stringWithFormat:@"%f", toneCurveInput0YValue];
+    
+    CGFloat toneCurveInput1XValue = _toneCurveInput1XSlider.value;
+    _toneCurveInput1XLabel.text = [NSString stringWithFormat:@"%f", toneCurveInput1XValue];
+    CGFloat toneCurveInput1YValue = _toneCurveInput1YSlider.value;
+    _toneCurveInput1YLabel.text = [NSString stringWithFormat:@"%f", toneCurveInput1YValue];
+    
+    CGFloat toneCurveInput2XValue = _toneCurveInput2XSlider.value;
+    _toneCurveInput2XLabel.text = [NSString stringWithFormat:@"%f", toneCurveInput2XValue];
+    CGFloat toneCurveInput2YValue = _toneCurveInput2YSlider.value;
+    _toneCurveInput2YLabel.text = [NSString stringWithFormat:@"%f", toneCurveInput2YValue];
+    
+    CGFloat toneCurveInput3XValue = _toneCurveInput3XSlider.value;
+    _toneCurveInput3XLabel.text = [NSString stringWithFormat:@"%f", toneCurveInput3XValue];
+    CGFloat toneCurveInput3YValue = _toneCurveInput3YSlider.value;
+    _toneCurveInput3YLabel.text = [NSString stringWithFormat:@"%f", toneCurveInput3YValue];
+    
+    CGFloat toneCurveInput4XValue = _toneCurveInput4XSlider.value;
+    _toneCurveInput4XLabel.text = [NSString stringWithFormat:@"%f", toneCurveInput4XValue];
+    CGFloat toneCurveInput4YValue = _toneCurveInput4YSlider.value;
+    _toneCurveInput4YLabel.text = [NSString stringWithFormat:@"%f", toneCurveInput4YValue];
+    
+    if (_isToneCurveEnabled) {
+        // Tone curveのための値を用意する
+        NSMutableArray *array = [[NSMutableArray alloc] init];
+        [array addObject:[CIVector vectorWithX:toneCurveInput0XValue Y:toneCurveInput0YValue]];
+        [array addObject:[CIVector vectorWithX:toneCurveInput1XValue Y:toneCurveInput1YValue]];
+        [array addObject:[CIVector vectorWithX:toneCurveInput2XValue Y:toneCurveInput2YValue]];
+        [array addObject:[CIVector vectorWithX:toneCurveInput3XValue Y:toneCurveInput3YValue]];
+        [array addObject:[CIVector vectorWithX:toneCurveInput4XValue Y:toneCurveInput4YValue]];
+        // Tone curveのフィルタをかける
+        filteredImage = [TTKEditImage imageFilterToneCurve:filteredImage withVectors:array];
     }
     
     self.imageView.image = filteredImage;
